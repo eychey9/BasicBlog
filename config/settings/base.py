6e5d8dfc005e29fb1861e488f3b58dc48db69800
dolcerellando <esempio@example.com>
2023-03-03 17:43:43 +0100

    customized commit message

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,6 +14,7 @@ import dj_database_url
 from dotenv import load_dotenv, find_dotenv 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -109,7 +110,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
